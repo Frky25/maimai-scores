6e2126099459473074d3365347d2aa75f5bc7ee8
Joshua Sloane <jsloane@mit.edu>
2019-10-13 14:01:44 -0700

    Testing function update/fix

--- testMaimai.py
+++ testMaimai.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 from procMaimai import processImg
 
 #['num_detect','ref_points', 'num_edges','title_processed']
@@ -6,9 +7,13 @@ def testAll(folder,imshows = []):
     scorepics = os.listdir(folder)
     for s in scorepics:
         print(s)
-        results = processImg(folder+'/'+s,imshows)
+        #load image
+        img = cv2.imread(folder+'/'+s,cv2.IMREAD_COLOR)
+        results = processImg(img,imshows)
         print(results)
 
 def testFile(file,imshows = []):
-    results = processImg(file,imshows)
+    #load image
+    img = cv2.imread(file,cv2.IMREAD_COLOR)
+    results = processImg(img,imshows)
     print(results)
